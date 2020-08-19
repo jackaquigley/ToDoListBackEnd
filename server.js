1c097f6ca2e8165312d9aae9eb9ca61ee1f86b5d
System Administrator <root@Jacks-MacBook-Pro.local>
2020-08-19 13:07:24 +0100

    set up server.js, created file structure and added .gitignore

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const parser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const createRouter = require('./helpers/create_router.js');
+const cors = require('cors')
+
+const app = express();
+
+MongoClient.connect('mongodb://localhost:27017')
+.then((client) => {
+  const db = client.db('todo_db');
+  const todoCollection = db.collection('todo');
+  const todoRouter = createRouter(todoCollection)
+  app.use('/api/todo', todoRouter);
+
+})
+
+app.use(parser.json());
+app.use(cors())
